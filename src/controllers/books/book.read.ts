fa3fb51e626e7e6910e23e148d1dca7144894077
Kazi Wakil Ahmed <ahmedwakil66@gmail.com>
2024-09-13 01:38:39 +0600

    View - book with author

--- src/controllers/books/book.read.ts
+++ src/controllers/books/book.read.ts
@@ -34,3 +34,37 @@ export const getBookById = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Failed to retrieve book" });
   }
 };
+
+// GET /books/:id/details
+export const getBookWithAuthor = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const bookWithAuthor = await knex("books")
+      .join("authors", "books.author_id", "authors.id")
+      .select(
+        "books.id as book_id",
+        "books.title",
+        "books.description",
+        "books.published_date",
+        "authors.id as author.id",
+        "authors.name as author.name",
+        "authors.bio as author.bio",
+        "authors.birthdate as author.birthdate",
+        // "authors.id as author_id",
+        // "authors.name as author_name",
+        // "authors.bio",
+        // "authors.birthdate",
+      )
+      .where("books.id", id)
+      .first();
+
+    if (!bookWithAuthor) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).json(bookWithAuthor);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to retrieve book and author" });
+  }
+};
